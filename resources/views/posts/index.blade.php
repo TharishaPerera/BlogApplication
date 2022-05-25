07cea4478a31b4f98f9c12b1ed74404ded21bb48
Baxbie <tharishaperera@gmail.com>
2022-05-25 16:13:31 +0530

    Post title link style change

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,7 @@
                             <img src="/storage/cover_images/{{$post->cover_image}}" style="width: 100%;">
                         </div>
                         <div class="col-md-8 clo-sm-4">
-                            <h3><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h3>
+                            <h3><a href="/posts/{{$post->id}}" style="font-weight: bold; text-decoration: none; color:rgb(27, 27, 27)">{{ $post->title }}</a></h3>
                             <small>Written on {{ $post->created_at }} by {{$post->user->name}}</small>
                         </div>
                     </div>
