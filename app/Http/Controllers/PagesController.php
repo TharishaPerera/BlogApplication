2e452adcc6a480d5510e167d2499648586d92c2a
Baxbie <tharishaperera@gmail.com>
2022-05-25 16:12:58 +0530

    add recent posts to index

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function index(){
         $title = "Welcome to Laravel !!";
+
+        $posts = Post::orderBy('created_at', 'desc')->take(3)->get();
         return view('pages/index')
-            ->with('title', $title);
+            ->with('title', $title)
+            ->with('posts', $posts);
     }
 
     public function about(){
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,16 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- <div class="jumbotron text-center"> --}}
-    <div class="p-5 mt-4 mb-4 bg-light rounded-3 text-center">
+    <div class="p-3 mt-4 mb-4 bg-light rounded text-center">
         <div class="container-fluid py-5">
-            <h1>Welcome to Laravel</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, dolor eligendi. Provident, impedit alias quae
+            <h1 class="index-h1">Dev.Talks</h1>
+            <p class="index-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, dolor eligendi.
+                Provident, impedit alias quae
                 at,
                 esse reiciendis quos assumenda distinctio cumque quia illum voluptas iure cum vero fugit beatae?</p>
-            <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg"
-                    href="/register" role="button">Register</a></p>
-
+            <p class="mt-5">
+                {{-- <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> --}}
+                <a class="btn btn-secondary btn-lg rounded-pill" href="/posts" role="button">Strat Reading</a>
+            </p>
         </div>
     </div>
+
+    
+    <div>
+        @if (count($posts) > 0)
+        <div class="p-3 mt-4 mb-4 text-center bg-light">
+            <h3>Recent Posts</h3>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            @foreach ($posts as $post)
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="/storage/cover_images/{{ $post->cover_image }}" style="width: 100%;">
+                        <div class="card-body">
+                            <h3><a href="/posts/{{ $post->id }}"
+                                    style="text-decoration: none; color:rgb(27, 27, 27)">{{ $post->title }}</a></h3>
+                            <small>Written on {{ $post->created_at }} by {{ $post->user->name }}</small>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        @endif
+    </div>
+
+    {{-- <div>
+        @if (count($posts) > 0)
+            @foreach ($posts as $post)
+                <div class="col-md-12">
+                    <div class="h-100 mt-3 p-4 bg-light border rounded-3">
+                        <div class="row">
+                            <div class="col-md-4 clo-sm-4">
+                                <img src="/storage/cover_images/{{ $post->cover_image }}" style="width: 100%;">
+                            </div>
+                            <div class="col-md-8 clo-sm-4">
+                                <h3><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h3>
+                                <small>Written on {{ $post->created_at }} by {{ $post->user->name }}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            <p>No posts found!</p>
+        @endif
+    </div> --}}
 @endsection
