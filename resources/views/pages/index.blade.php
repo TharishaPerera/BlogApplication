6797be201433a524ff23fb6b2e68173b7c475cc8
Baxbie <tharishaperera@gmail.com>
2022-05-25 16:20:38 +0530

    remove unwanted comments

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,7 +10,7 @@
                 esse reiciendis quos assumenda distinctio cumque quia illum voluptas iure cum vero fugit beatae?</p>
             <p class="mt-5">
                 {{-- <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> --}}
-                <a class="btn btn-secondary btn-lg rounded-pill" href="/posts" role="button">Strat Reading</a>
+                <a class="btn btn-secondary btn-lg rounded-pill px-4" href="/posts" role="button">Strat Reading</a>
             </p>
         </div>
     </div>
@@ -37,26 +37,4 @@
         </div>
         @endif
     </div>
-
-    {{-- <div>
-        @if (count($posts) > 0)
-            @foreach ($posts as $post)
-                <div class="col-md-12">
-                    <div class="h-100 mt-3 p-4 bg-light border rounded-3">
-                        <div class="row">
-                            <div class="col-md-4 clo-sm-4">
-                                <img src="/storage/cover_images/{{ $post->cover_image }}" style="width: 100%;">
-                            </div>
-                            <div class="col-md-8 clo-sm-4">
-                                <h3><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h3>
-                                <small>Written on {{ $post->created_at }} by {{ $post->user->name }}</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        @else
-            <p>No posts found!</p>
-        @endif
-    </div> --}}
 @endsection
